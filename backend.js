bbb67c05c787eb48e84060b74693a1b2a883378a
OzzY <ewagne02@calpoly.edu>
2022-09-26 13:32:41 -0700

    users

--- backend.js
+++ backend.js
@@ -1,5 +1,4 @@
 //imported express module for http calls to route
-//bruh
 const express = require('express');
 //instance of express
 const app = express();
@@ -16,4 +15,41 @@ app.get('/', (req, res) => {
 // backend server listneing for requests
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});      
+});  
+
+//list of users in JSON format
+const users = { 
+    users_list :
+    [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+}
+
+//returns entire list of users
+app.get('/users', (req, res) => {
+    res.send(users);
+});
